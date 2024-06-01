8714c52b6a2102a38e632810fada60a4ff8bc21e
Logali-Group <training@logaligroup.com>
2024-06-01 10:48:22 -0500

    Estructura del Proyecto

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("b23c234.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"b23c234/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
